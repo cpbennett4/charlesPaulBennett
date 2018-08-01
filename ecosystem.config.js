15f0777e7492a31ecf630b0a256038c2a90292f3
Charles Paul Bennett <cpbennett4@gmail.com>
2018-08-01 13:38:19 -0700

    Change ssh user from ubuntu to chucky in appropriate places

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,12 +5,12 @@ module.exports = {
   }],
   deploy: {
     production: {
-      user: 'ubuntu',
+      user: 'chucky',
       host: 'ec2-54-193-121-79.us-west-1.compute.amazonaws.com',
       key: '~/.ssh/charlesPaulBennett.pem',
       ref: 'origin/master',
       repo: 'git@github.com:cpbennett4/charlesPaulBennett.git',
-      path: '/home/ubuntu/charlesPaulBennett',
+      path: '/home/chucky/charlesPaulBennett',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
