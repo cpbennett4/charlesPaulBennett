1200b0682ad3954b266021b6d43c3c88b6721c33
Charles Paul Bennett <cpbennett4@gmail.com>
2018-08-03 16:48:37 -0700

    Insert "apps" in path string

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       key: '~/.ssh/charlesPaulBennett/charlesPaulBennett',
       ref: 'origin/master',
       repo: 'git@github.com:cpbennett4/charlesPaulBennett.git',
-      path: '/home/chucky/charlesPaulBennett',
+      path: '/home/chucky/apps/charlesPaulBennett',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
