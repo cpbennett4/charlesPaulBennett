709b9bd0e7875ef384823cdfd376b96331b1fbd5
Charles Paul Bennett <cpbennett4@gmail.com>
2018-08-04 15:57:45 -0700

    Install and implement serve-favicon module

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const favicon = require('serve-favicon');
 
 const app = express();
+app.use(favicon(path.join(__dirname, '..', 'favicon.ico')));
+
 
 const port = process.env.PORT || 3000;
 
