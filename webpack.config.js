65a893c0b0661091954a219ef8e608320e5e0981
Charles Paul Bennett <cpbennett4@gmail.com>
2018-08-05 15:33:06 -0700

    Initialize React, Webpack, Babel

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'client/public');
+const APP_DIR = path.resolve(__dirname, 'client/src');
+
+module.exports = {
+  mode: 'development',
+  // overrides default of current working drive
+  context: APP_DIR,
+
+  entry: [
+    `${APP_DIR}/index.jsx`
+  ],
+
+  // accurate source map that supports minimizing, though slow
+  devtool: 'inline-source-map',
+
+  watch: true,
+
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['env', 'react'],
+        },
+      },
+    ],
+  },
+  resolve: {
+    modules: [
+      // doesn't seem to work
+      'client/src',
+      // works fine
+      'node_modules',
+    ],
+    // .scss extensions don't resolve for some reason
+    extensions: ['*', '.js', '.jsx', '.css', '.scss', '.sass'],
+  },
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+};
