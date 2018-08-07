ab9c8efa2dcccac86aafeea670ff3deedf3a7c24
Charles Paul Bennett <cpbennett4@gmail.com>
2018-08-07 16:44:28 -0700

    Put quote with basic styling on Home view

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Home = () => (
   <div className="Home">
-    <h1>
-      test from Home
-    </h1>
+    <h2>
+      &quot;I love Software Engineering because I enjoy riddles and puzzles.
+      While building an approach to a solution, I get to enjoy a riddle;
+      While implementing the approach, I get to put build a puzzle.&quot;
+    </h2>
   </div>
 );
 
