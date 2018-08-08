dbd569f14a538a71c8aa97428af295bdd3ebf23d
Charles Paul Bennett <cpbennett4@gmail.com>
2018-08-08 15:28:06 -0700

    Implement fontAwesome library in index.js, install solid icons

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import App from './containers/App';
 
 require('./style/styles.scss');
+
+library.add(fab, faBars);
 
 ReactDom.render(
   <BrowserRouter>
