07854355561d732ebfc7fb7b03f8e064ca0c359b
Charles Paul Bennett <cpbennett4@gmail.com>
2018-08-09 15:50:49 -0700

    Correct landing statement

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Home = () => (
   <div className="Home">
     <h2>
-      &quot;I love Software Engineering because I enjoy riddles and puzzles.
+      I love Software Engineering because I love riddles and puzzles.
       While building an approach to a solution, I get to enjoy a riddle;
-      While implementing the approach, I get to put build a puzzle.&quot;
+      While implementing the approach, I get to enjoy building a puzzle.
     </h2>
   </div>
 );
