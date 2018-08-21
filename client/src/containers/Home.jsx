d048ca2bfea485f98fd3c3bba0c3e1a6e5de724a
Charles Paul Bennett <cpbennett4@gmail.com>
2018-08-21 14:01:10 -0700

    Edit css variables, tweak header, style home content

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -2,12 +2,35 @@ import React from 'react';
 
 const Home = () => (
   <div className="Home">
-    <h2>
-      I love Software Engineering because I love riddles and puzzles.
-      While building an approach to a solution, I get to enjoy a riddle;
-      While implementing the approach, I get to enjoy building a puzzle.
-    </h2>
+    <div className="jumboTron">
+      <div className="container">
+        <h1>
+          Charles Paul Bennett, at your service.
+        </h1>
+        <h2>
+          Most people just call me Chucky.
+        </h2>
+        <h3>
+          I work with those whose passions intersect with my own in a relentlesss
+          pursuit of improving the world around me.
+        </h3>
+        <div className="CTAButtons">
+          <button type="button">
+            View Resume
+          </button>
+          <button type="button">
+            Contact Me
+          </button>
+        </div>
+      </div>
+      <div className="skillsLead">
+        <h3>
+          My most heavily relied on skills are empathy, collaboration & Javascript.
+        </h3>
+      </div>
+    </div>
   </div>
 );
+
 
 export default Home;
