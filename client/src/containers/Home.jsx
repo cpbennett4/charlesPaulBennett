853b9ae854403bb3b63b711bb1a4ba856b7d5273
Charles Paul Bennett <cpbennett4@gmail.com>
2018-08-30 20:08:29 -0700

    Move Pdf to own component, include ability to show/remove Pdf component

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,64 +1,61 @@
 import React from 'react';
-import { Document, Page } from 'react-pdf/dist/entry.webpack';
-import myResume from '../style/assets/Resume.pdf';
-import 'react-pdf/dist/Page/AnnotationLayer.css';
+import Pdf from '../components/Pdf';
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      numPages: null,
-      pageNumber: 1,
+      showPdf: false,
     };
 
-    this.onDocumentLoadSuccess = ({ numPages }) => {
-      this.setState({ numPages });
+    this.showPdf = () => {
+      this.setState({ showPdf: true });
+    };
+
+    this.removePdf = () => {
+      this.setState({ showPdf: false });
     };
   }
 
   render() {
-    const { numPages, pageNumber } = this.state;
-    return (
-      <div className="Home">
-        <div className="jumboTron">
-          <div className="container">
-            <h1>
-              Charles Paul Bennett, at your service.
-            </h1>
-            <h2>
-              Most people just call me Chucky.
-            </h2>
-            <h3>
-              I work with those whose passions intersect with my own in a relentlesss
-              pursuit of improving the world around me.
-            </h3>
-            <div className="CTAButtons">
-              <button type="button">
-                View Resume
-              </button>
-              <Document
-                file={myResume}
-                onLoadSuccess={this.onDocumentLoadSuccess}
-              >
-                <Page pageNumber={pageNumber} />
-              </Document>
-              <p>
-                Page
-                {pageNumber}
-                of
-                {numPages}
-              </p>
-              <button type="button">
-                Contact Me
-              </button>
-            </div>
-          </div>
-          <div className="skillsLead">
-            <h3>
-              My most heavily relied on skills are empathy, collaboration & Javascript.
-            </h3>
+    const { showPdf } = this.state;
+    const home = (
+      <div className="jumboTron">
+        <div className="container">
+          <h1>
+            Charles Paul Bennett, at your service.
+          </h1>
+          <h2>
+            Most people just call me Chucky.
+          </h2>
+          <h3>
+            I work with those whose passions intersect with my own in a relentlesss
+            pursuit of improving the world around me.
+          </h3>
+          <div className="CTAButtons">
+            <button
+              type="button"
+              onClick={this.showPdf}
+            >
+              View Resume
+            </button>
+            <button type="button">
+              Contact Me
+            </button>
           </div>
         </div>
+        <div className="skillsLead">
+          <h3>
+            My most heavily relied on skills are empathy, collaboration & Javascript.
+          </h3>
+        </div>
+      </div>
+    );
+    return (
+      <div className="Home">
+        {
+          showPdf ? <Pdf removePdf={this.removePdf} /> : home
+        }
       </div>
     );
   }
