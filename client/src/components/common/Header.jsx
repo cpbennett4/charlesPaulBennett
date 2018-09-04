4aef68267e26222227e4b48338c187a977656f27
Charles Paul Bennett <cpbennett4@gmail.com>
2018-09-04 16:36:37 -0700

    Call removePdf() onClick of logo and 'Home' link

--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PropTypes from 'prop-types';
 import $ from 'jquery';
 
 const toggleShowing = () => {
@@ -12,72 +13,86 @@ const toggleShowing = () => {
   }
 };
 
-const Header = () => (
-  <div className="Header">
-    <button className="Handle" type="button" onClick={toggleShowing}>
-      <h3>
-        Menu
-      </h3>
-      <FontAwesomeIcon icon="bars" />
-    </button>
-    <nav className="Nav">
-      <div className="Logo">
-        <h1>
-          <Link to="/">
-            CPB
-          </Link>
-        </h1>
-      </div>
-      <div className="Menu">
-        <ul>
-          <li>
-            <NavLink exact to="/" onClick={toggleShowing}>
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/About" onClick={toggleShowing}>
-              About
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/Portfolio" onClick={toggleShowing}>
-              Portfolio
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/Contact" onClick={toggleShowing}>
-              Contact
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-      <div className="Social">
-        <ul>
-          <li id="linkedin">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="http://www.linkedin.com/in/cpbennett4"
-              onClick={toggleShowing}
-            >
-              <FontAwesomeIcon size="lg" icon={['fab', 'linkedin']} />
-            </a>
-          </li>
-          <li id="github">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="http://www.github.com/cpbennett4"
-              onClick={toggleShowing}
-            >
-              <FontAwesomeIcon size="lg" icon={['fab', 'github']} />
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </div>
-);
+const Header = (props) => {
+  const { removePdf } = props;
+  return (
+    <div className="Header">
+      <button className="Handle" type="button" onClick={toggleShowing}>
+        <h3>
+          Menu
+        </h3>
+        <FontAwesomeIcon icon="bars" />
+      </button>
+      <nav className="Nav">
+        <div className="Logo">
+          <h1>
+            <Link to="/" onClick={removePdf}>
+              CPB
+            </Link>
+          </h1>
+        </div>
+        <div className="Menu">
+          <ul>
+            <li>
+              <NavLink
+                exact
+                to="/"
+                onClick={() => {
+                  removePdf();
+                  toggleShowing();
+                }}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/About" onClick={toggleShowing}>
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/Portfolio" onClick={toggleShowing}>
+                Portfolio
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/Contact" onClick={toggleShowing}>
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="Social">
+          <ul>
+            <li id="linkedin">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="http://www.linkedin.com/in/cpbennett4"
+                onClick={toggleShowing}
+              >
+                <FontAwesomeIcon size="lg" icon={['fab', 'linkedin']} />
+              </a>
+            </li>
+            <li id="github">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="http://www.github.com/cpbennett4"
+                onClick={toggleShowing}
+              >
+                <FontAwesomeIcon size="lg" icon={['fab', 'github']} />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+Header.propTypes = {
+  removePdf: PropTypes.func.isRequired,
+};
 
 export default Header;
