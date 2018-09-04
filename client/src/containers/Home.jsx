11c9104fb6f39156c9531900119c45a9d4a0cd47
Charles Paul Bennett <cpbennett4@gmail.com>
2018-09-04 16:37:14 -0700

    Convert to stateless, setup use of Pdf state/methods from props

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,64 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Pdf from '../components/Pdf';
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showPdf: false,
-    };
-
-    this.showPdf = () => {
-      this.setState({ showPdf: true });
-    };
-
-    this.removePdf = () => {
-      this.setState({ showPdf: false });
-    };
-  }
-
-  render() {
-    const { showPdf } = this.state;
-    const home = (
-      <div className="jumboTron">
-        <div className="container">
-          <h1>
-            Charles Paul Bennett, at your service.
-          </h1>
-          <h2>
-            Most people just call me Chucky.
-          </h2>
-          <h3>
-            I work with those whose passions intersect with my own in a relentlesss
-            pursuit of improving the world around me.
-          </h3>
-          <div className="CTAButtons">
-            <button
-              type="button"
-              onClick={this.showPdf}
-            >
-              View Resume
-            </button>
-            <button type="button">
-              Contact Me
-            </button>
-          </div>
-        </div>
-        <div className="skillsLead">
-          <h3>
-            My most heavily relied on skills are empathy, collaboration & Javascript.
-          </h3>
+const Home = (props) => {
+  const { showingPdf, showPdf, removePdf } = props;
+  const home = (
+    <div className="jumboTron">
+      <div className="container">
+        <h1>
+          Charles Paul Bennett, at your service.
+        </h1>
+        <h2>
+          Most people just call me Chucky.
+        </h2>
+        <h3>
+          I work with those whose passions intersect with my own in a relentlesss
+          pursuit of improving the world around me.
+        </h3>
+        <div className="CTAButtons">
+          <button
+            type="button"
+            onClick={showPdf}
+          >
+            View Resume
+          </button>
+          <button type="button">
+            Contact Me
+          </button>
         </div>
       </div>
-    );
-    return (
-      <div className="Home">
-        {
-          showPdf ? <Pdf removePdf={this.removePdf} /> : home
-        }
+      <div className="skillsLead">
+        <h3>
+          My most heavily relied on skills are empathy, collaboration & Javascript.
+        </h3>
       </div>
-    );
-  }
-}
+    </div>
+  );
+  return (
+    <div className="Home">
+      {
+        showingPdf ? <Pdf removePdf={removePdf} /> : home
+      }
+    </div>
+  );
+};
+
+Home.propTypes = {
+  showingPdf: PropTypes.bool.isRequired,
+  showPdf: PropTypes.func.isRequired,
+  removePdf: PropTypes.func.isRequired,
+};
 
 export default Home;
